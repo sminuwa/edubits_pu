59db1ca98fea215285bc170b5e869a50418d1869
Sunusi Mohd Inuwa <sminuwa@yahoo.com>
2019-01-17 14:23:18 +0100

    changing interface for application form

---
+++ admission/index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Virtual School Manager">
+    <meta name="author" content="Fokalbits">
+    <title>EduBits - Home</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+    <!--Animate headline css-->
+    <link href="assets/cd-intro/headline.css" rel="stylesheet">
+    <!-- BASE CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+	<link href="css/vendors.css" rel="stylesheet">
+	<link href="css/icon_fonts/css/all_icons.min.css" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+	
+	<div id="preloader">
+		<div data-loader="circle-side"></div>
+	</div>
+	<!-- End Preload -->
+	
+	<header class="header fadeInDown">
+		<div id="logo">
+			<a href="index.php"><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></a>
+		</div>
+		<ul id="top_menu">
+			<li><a href="login.php" class="login">Login</a></li>
+			<li><a href="#0" class="search-overlay-menu-btn">Search</a></li>
+			<li class="hidden_tablet"><a href="#0">Send us feedback</a></li>
+			<li class="hidden_tablet"><a href="apply/" class="btn_1 rounded">Admission</a></li>
+			<li>
+				<div class="hamburger hamburger--spin">
+					<div class="hamburger-box">
+						<div class="hamburger-inner"></div>
+					</div>
+				</div>
+			</li>
+		</ul>
+		<!-- /top_menu -->
+	</header>
+	<!-- /header -->
+	
+	<div id="main_menu">
+		<div class="container">
+			<nav class="version_2">
+				<!--<div class="row">
+					<div class="col-md-3">
+						<h3>Home</h3>
+						<ul>
+							<li><a href="index.php">Home version 1</a></li>
+							<li><a href="index-2.php">Home version 2</a></li>
+							<li><a href="index-3.php">Home version 3</a></li>
+							<li><a href="index-4.php">Home version 4</a></li>
+							<li><a href="index-5.php">With Cookie bar (EU law)</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<h3>Courses</h3>
+						<ul>
+							<li><a href="courses-grid.php">Courses grid</a></li>
+							<li><a href="courses-grid-sidebar.php">Courses grid sidebar</a></li>
+							<li><a href="courses-list.php">Courses list</a></li>
+							<li><a href="courses-list-sidebar.php">Courses list sidebar</a></li>
+							<li><a href="course-detail.php">Course detail</a></li>
+							<li><a href="course-detail-2.php">Course detail (working form)</a></li>
+							<li><a href="admission.php">Admission wizard</a></li>
+							<li><a href="teacher-detail.php">Teacher detail</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<h3>Pages</h3>
+						<ul>
+							<li><a href="#0">Menu 1</a><span class="badge_info">New</span></li>
+							<li><a href="about.php">About</a></li>
+							<li><a href="blog.php">Blog</a></li>
+							<li><a href="login.php">Login</a></li>
+							<li><a href="register.php">Register</a></li>
+							<li><a href="contacts.php">Contacts</a></li>
+							<li><a href="404.php">404 page</a></li>
+							<li><a href="agenda-calendar.php">Agenda Calendar</a></li>
+							<li><a href="faq.php">Faq</a></li>
+							<li><a href="help.php">Help</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<h3>Extra pages</h3>
+						<ul>
+							<li><a href="media-gallery.php">Media gallery</a></li>
+							<li><a href="cart-1.php">Cart page 1</a></li>
+							<li><a href="cart-2.php">Cart page 2</a></li>
+							<li><a href="cart-3.php">Cart page 3</a></li>
+							<li><a href="pricing-tables.php">Responsive pricing tables</a></li>
+							<li><a href="coming_soon/index.php">Coming soon</a></li>
+							<li><a href="icon-pack-1.php">Icon pack 1</a></li>
+							<li><a href="icon-pack-2.php">Icon pack 2</a></li>
+							<li><a href="icon-pack-3.php">Icon pack 3</a></li>
+							<li><a href="icon-pack-4.php">Icon pack 4</a></li>
+						</ul>
+					</div>
+				</div>-->
+				<!-- /row -->
+			</nav>
+			<div class="follow_us">
+				<ul>
+					<li>Follow us</li>
+					<li><a href="#0"><i class="ti-facebook"></i></a></li>
+					<li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
+					<li><a href="#0"><i class="ti-google"></i></a></li>
+					<li><a href="#0"><i class="ti-pinterest"></i></a></li>
+					<li><a href="#0"><i class="ti-instagram"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- /main_menu 2-->
+	
+	<main>
+		<section class="hero_single version_2">
+			<div class="wrapper">
+				<div class="container">
+                    <div class="slider-sec-text">
+                        <div class="cd-intro" style="text-align:center;margin: auto; ">
+                            <h3 class="cd-headline clip is-full-width font-28 color-b font-w-8 ptb-9" >
+                                <span class="cd-words-wrapper">
+                                    <b class="is-visible">Get involved in your child's education</b>
+                                    <b>Stay updated on your child progress</b>
+                                    <b>Pay fees effortless</b>
+                                    <b>With Seamless virtual school manager</b>
+                                </span>
+                            </h3>
+                        </div>
+                    </div>
+
+					<!--<p>Stay ahead with the 1st seamless virtual school manager</p>-->
+					<p>Applying to Nursery, Primary or Secondary school means more than just filling out forms. <br>
+                        For a successful school application, you first need to understand each school’s admission requirements, gather information, meet deadlines, and pay any necessary fees.
+                        Plus, each school has different application requirements and deadlines, so it’s important to get organized.</p>
+					<form>
+						<div id="custom-search-input">
+							<div class="input-group">
+								<input type="text" class=" search-query" placeholder="Search for schools">
+								<input type="submit" class="btn_search" value="Search">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
+		<!-- /hero_single -->
+
+		<div class="features clearfix">
+			<div class="container">
+				<ul>
+					<li><i class="pe-7s-study"></i>
+						<h4>+100 Nursery Schools</h4><span>Explore a variety of fresh topics</span>
+					</li>
+					<li><i class="pe-7s-cup"></i>
+						<h4>+200 Primary Schools</h4><span>Find the right instructor for you</span>
+					</li>
+					<li><i class="pe-7s-target"></i>
+						<h4>+500 Secondary Schools</h4><span>Increase your personal expertise</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<!-- /features -->
+
+
+
+		<div class="bg_color_1">
+			<div class="container margin_120_95">
+				<div class="main_title_2">
+					<span><em></em></span>
+					<h2>Helping schools grow efficiently</h2>
+					<p>EduBits empowers Educators to build fast-growing, efficient, and stress-free schools,
+                        by combining all tools necessary for running a modern school into one
+                        platform that is simple, flexible, and seamless. </p>
+				</div>
+
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /bg_color_1 -->
+
+		<div class="call_section">
+			<div class="container clearfix">
+				<div class="col-lg-5 col-md-6 float-right wow" data-wow-offset="250">
+					<div class="block-reveal">
+						<div class="block-vertical"></div>
+						<div class="box_1">
+							<h3>The 1st Virtual School Manager</h3>
+							<p>EduBits is built with the latest technological tools,
+                                standards and platforms, guaranteeing security,
+                                speed and highest percentage uptime. </p>
+							<a href="#0" class="btn_1 rounded">Read more</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/call_section-->
+	</main>
+	<!-- /main -->
+
+	<footer>
+		<div class="container margin_120_95">
+			<div class="row">
+				<div class="col-lg-5 col-md-12 p-r-5">
+					<p><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></p>
+					<p>EduBits exist to make technology solutions that facilitate learning and streamline
+                        school operations available to all schools. We see ourselves as technical
+                        partners to schools, providing them with technology solutions that help
+                        them grow quickly and profitably, while improving student outcomes.
+                        We maintain close working relationship with partner schools to ensure
+                        technology adoption is effective and painless.</p>
+					<div class="follow_us">
+						<ul>
+							<li>Follow us</li>
+							<li><a href="#0"><i class="ti-facebook"></i></a></li>
+							<li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
+							<li><a href="#0"><i class="ti-google"></i></a></li>
+							<li><a href="#0"><i class="ti-pinterest"></i></a></li>
+							<li><a href="#0"><i class="ti-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 ml-lg-auto">
+					<h5>Useful links</h5>
+					<ul class="links">
+						<li><a href="apply/">Admission</a></li>
+						<li><a href="#0">About</a></li>
+						<li><a href="#0">Login</a></li>
+						<li><a href="#0">Register</a></li>
+						<li><a href="#0">News &amp; Events</a></li>
+						<li><a href="#0">Contacts</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h5>Contact with Us</h5>
+					<ul class="contacts">
+						<li><a href="tel://+2348034904618"><i class="ti-mobile"></i> + (234) 803 490 4618</a></li>
+						<li><a href="mailto:info@edubits.ng"><i class="ti-email"></i> info@edubits.ng</a></li>
+					</ul>
+					<div id="newsletter">
+					<h6>Newsletter</h6>
+					<div id="message-newsletter"></div>
+					<form method="post" action="assets/newsletter.php" name="newsletter_form" id="newsletter_form">
+						<div class="form-group">
+							<input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
+							<input type="submit" value="Submit" id="submit-newsletter">
+						</div>
+					</form>
+					</div>
+				</div>
+			</div>
+			<!--/row-->
+			<hr>
+			<div class="row">
+				<div class="col-md-8">
+					<ul id="additional_links">
+						<li><a href="#0">Terms and conditions</a></li>
+						<li><a href="#0">Privacy</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<div id="copy">© <?php echo date("Y");?> EduBits</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--/footer-->
+	
+	<!-- Search Menu -->
+	<div class="search-overlay-menu">
+		<span class="search-overlay-close"><span class="closebt"><i class="ti-close"></i></span></span>
+		<form role="search" id="searchform" method="get">
+			<input value="" name="q" type="search" placeholder="Search..." />
+			<button type="submit"><i class="icon_search"></i>
+			</button>
+		</form>
+	</div><!-- End Search Menu -->
+	
+	<!-- COMMON SCRIPTS -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/common_scripts.js"></script>
+    <script src="js/main.js"></script>
+	<script src="assets/validate.js"></script>
+    <script src="assets/cd-intro/main.js"></script>
+</body>
+</html>
